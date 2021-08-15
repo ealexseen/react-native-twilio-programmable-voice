c4f418f719960243930a985ba59051938e11f6a8
Eugene Alexeenko <yauheni.aliakseyenko@neklo.com>
2021-08-15 18:10:41 +0300

    update intercome

--- android/src/main/java/com/hoxfon/react/RNTwilioVoice/fcm/VoiceFirebaseMessagingService.java
+++ android/src/main/java/com/hoxfon/react/RNTwilioVoice/fcm/VoiceFirebaseMessagingService.java
@@ -30,8 +30,12 @@ import java.util.Map;
 import java.util.Random;
 
 import static com.hoxfon.react.RNTwilioVoice.TwilioVoiceModule.TAG;
+import static com.dieam.reactnativepushnotification.modules.RNPushNotification.LOG_TAG;
+
+import io.intercom.android.sdk.push.IntercomPushClient;
 
 public class VoiceFirebaseMessagingService extends FirebaseMessagingService {
+    private final IntercomPushClient intercomPushClient = new IntercomPushClient();
 
     @Override
     public void onCreate() {
@@ -41,6 +45,12 @@ public class VoiceFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onNewToken(String token) {
         super.onNewToken(token);
+
+        Log.d(LOG_TAG, "onNewToken FirebaseMessagingIdService: " + refreshedToken);
+
+        intercomPushClient.sendTokenToIntercom(getApplication(), refreshedToken);
+        //DO HOST LOGIC HERE
+
         Intent intent = new Intent(Constants.ACTION_FCM_TOKEN);
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
@@ -52,76 +62,85 @@ public class VoiceFirebaseMessagingService extends FirebaseMessagingService {
      */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "Bundle data: " + remoteMessage.getData());
-        }
+        super.onMessageReceived(remoteMessage);
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Map<String, String> data = remoteMessage.getData();
+        Log.d(LOG_TAG, "onMessageReceived FirebaseMessagingIdService: " + remoteMessage);
 
-            // If notification ID is not provided by the user for push notification, generate one at random
-            Random randomNumberGenerator = new Random(System.currentTimeMillis());
-            final int notificationId = randomNumberGenerator.nextInt();
-
-            boolean valid = Voice.handleMessage(this, data, new MessageListener() {
-                @Override
-                public void onCallInvite(final CallInvite callInvite) {
-                    // We need to run this on the main thread, as the React code assumes that is true.
-                    // Namely, DevServerHelper constructs a Handler() without a Looper, which triggers:
-                    // "Can't create handler inside thread that has not called Looper.prepare()"
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.post(new Runnable() {
-                        public void run() {
-                            CallNotificationManager callNotificationManager = new CallNotificationManager();
-                            // Construct and load our normal React JS code bundle
-                            ReactInstanceManager mReactInstanceManager = ((ReactApplication) getApplication()).getReactNativeHost().getReactInstanceManager();
-                            ReactContext context = mReactInstanceManager.getCurrentReactContext();
-
-                            // initialise appImportance to the highest possible importance in case context is null
-                            int appImportance = ActivityManager.RunningAppProcessInfo.IMPORTANCE_GONE;
-
-                            if (context != null) {
-                                appImportance = callNotificationManager.getApplicationImportance((ReactApplicationContext)context);
-                            }
-                            if (BuildConfig.DEBUG) {
-                                Log.d(TAG, "context: " + context + ". appImportance = " + appImportance);
-                            }
-
-                            // when the app is not started or in the background
-                            if (appImportance > ActivityManager.RunningAppProcessInfo.IMPORTANCE_VISIBLE) {
-                                if (BuildConfig.DEBUG) {
-                                    Log.d(TAG, "Background");
-                                }
-                                handleInvite(callInvite, notificationId);
-                                return;
-                            }
-
-                            Intent intent = new Intent(Constants.ACTION_INCOMING_CALL);
-                            intent.putExtra(Constants.INCOMING_CALL_NOTIFICATION_ID, notificationId);
-                            intent.putExtra(Constants.INCOMING_CALL_INVITE, callInvite);
-                            LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
-                        }
-                    });
-                }
-
-                @Override
-                public void onCancelledCallInvite(@NonNull CancelledCallInvite cancelledCallInvite, @Nullable CallException callException) {
-                    // The call is prematurely disconnected by the caller.
-                    // The callee does not accept or reject the call within 30 seconds.
-                    // The Voice SDK is unable to establish a connection to Twilio.
-                    handleCancelledCallInvite(cancelledCallInvite, callException);
-                }
-            });
-
-            if (!valid) {
-                Log.e(TAG, "The message was not a valid Twilio Voice SDK payload: " + remoteMessage.getData());
+        Map message = remoteMessage.getData();
+        if (intercomPushClient.isIntercomPush(message)) {
+            intercomPushClient.handlePush(getApplication(), message);
+        } else {
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, "Bundle data: " + remoteMessage.getData());
             }
-        }
 
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.e(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            // Check if message contains a data payload.
+            if (remoteMessage.getData().size() > 0) {
+                Map<String, String> data = remoteMessage.getData();
+
+                // If notification ID is not provided by the user for push notification, generate one at random
+                Random randomNumberGenerator = new Random(System.currentTimeMillis());
+                final int notificationId = randomNumberGenerator.nextInt();
+
+                boolean valid = Voice.handleMessage(this, data, new MessageListener() {
+                    @Override
+                    public void onCallInvite(final CallInvite callInvite) {
+                        // We need to run this on the main thread, as the React code assumes that is true.
+                        // Namely, DevServerHelper constructs a Handler() without a Looper, which triggers:
+                        // "Can't create handler inside thread that has not called Looper.prepare()"
+                        Handler handler = new Handler(Looper.getMainLooper());
+                        handler.post(new Runnable() {
+                            public void run() {
+                                CallNotificationManager callNotificationManager = new CallNotificationManager();
+                                // Construct and load our normal React JS code bundle
+                                ReactInstanceManager mReactInstanceManager = ((ReactApplication) getApplication()).getReactNativeHost().getReactInstanceManager();
+                                ReactContext context = mReactInstanceManager.getCurrentReactContext();
+
+                                // initialise appImportance to the highest possible importance in case context is null
+                                int appImportance = ActivityManager.RunningAppProcessInfo.IMPORTANCE_GONE;
+
+                                if (context != null) {
+                                    appImportance = callNotificationManager.getApplicationImportance((ReactApplicationContext)context);
+                                }
+                                if (BuildConfig.DEBUG) {
+                                    Log.d(TAG, "context: " + context + ". appImportance = " + appImportance);
+                                }
+
+                                // when the app is not started or in the background
+                                if (appImportance > ActivityManager.RunningAppProcessInfo.IMPORTANCE_VISIBLE) {
+                                    if (BuildConfig.DEBUG) {
+                                        Log.d(TAG, "Background");
+                                    }
+                                    handleInvite(callInvite, notificationId);
+                                    return;
+                                }
+
+                                Intent intent = new Intent(Constants.ACTION_INCOMING_CALL);
+                                intent.putExtra(Constants.INCOMING_CALL_NOTIFICATION_ID, notificationId);
+                                intent.putExtra(Constants.INCOMING_CALL_INVITE, callInvite);
+                                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelledCallInvite(@NonNull CancelledCallInvite cancelledCallInvite, @Nullable CallException callException) {
+                        // The call is prematurely disconnected by the caller.
+                        // The callee does not accept or reject the call within 30 seconds.
+                        // The Voice SDK is unable to establish a connection to Twilio.
+                        handleCancelledCallInvite(cancelledCallInvite, callException);
+                    }
+                });
+
+                if (!valid) {
+                    Log.e(TAG, "The message was not a valid Twilio Voice SDK payload: " + remoteMessage.getData());
+                }
+            }
+
+            // Check if message contains a notification payload.
+            if (remoteMessage.getNotification() != null) {
+                Log.e(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            }
         }
     }
 
